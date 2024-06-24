3930d4c3a7775d0e47375af73f9e143ec742ef5b
박상준 <80584060+qkrtkdwns3410@users.noreply.github.com>
2024-06-24 21:21:49 +0900

    feat: 토비의 스프링부트 Env

--- src/main/java/com/company/tobispringboot/TobiSpringBootApplication.java
+++ src/main/java/com/company/tobispringboot/TobiSpringBootApplication.java
@@ -1,9 +1,20 @@
 package com.company.tobispringboot;
 
 import com.company.config.MySpringBootApplication;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 @MySpringBootApplication
 public class TobiSpringBootApplication {
+    
+    @Bean
+    ApplicationRunner applicationRunner(Environment env) {
+        return args -> {
+            env.getProperty("my.name");
+            System.out.println("Hello, " + env.getProperty("my.name"));
+        };
+    }
     
     public static void main(String[] args) {
         MySpringApplication.run(TobiSpringBootApplication.class, args);
